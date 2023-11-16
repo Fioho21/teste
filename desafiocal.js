292da1fa3c288224b011c6364f5f8066097d4345
Diogo Silva <145931539+Fioho21@users.noreply.github.com>
2023-11-16 15:03:53 -0300

    Update desafiocal.js

--- desafiocal.js
+++ desafiocal.js
@@ -3,25 +3,25 @@ function matches(vitoria, derrota) {
     return resultado
   }
   
-  function rank() {
-    let resultRank = matches(50, 20)
+  function podium() {
+    let podiumResult = matches(100, 10)
     let nivel = ""
-    if (resultRank <= 10) {
+    if (podiumResult <= 10) {
       nivel = "Ferro"
-    } else if (resultRank <= 20) {
-      nivel = "Bronze"
-    } else if (resultRank <= 50) {
-      nivel = "Prata"
-    } else if (resultRank <= 80) {
-      nivel = "Ouro"
-    } else if (resultRank <= 90) {
-      nivel = "Diamante"
-    } else if (resultRank <= 100) {
-      nivel = "Lendário"
-    } else if (resultRank >= 100) {
-      nivel = "Imortal"
+     } else if (podiumResult <= 20) {
+          nivel = "Bronze"
+        } else if (podiumResult <= 50) {
+              nivel = "Prata"
+            } else if (podiumResult <= 80) {
+                  nivel = "Ouro"
+                } else if (podiumResult <= 90) {
+                      nivel = "Diamante"
+                    } else if (podiumResult <= 100) {
+                          nivel = "Lendário"
+                         } else if (podiumResult >= 100) {
+                              nivel = "Imortal"
     }
-    return `O Herói tem de saldo de ${resultRank} e está no nível de ${nivel}`
+    return `O Herói tem de saldo de ${podiumResult} e está no nível de ${nivel}`
   }
   
-  console.log(rank())
+  console.log(podium())
